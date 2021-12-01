d80efe058cec8eaf2aff421c70182fed923b73e7
Withos <withos3@gmail.com>
2021-12-01 22:02:55 +0100

    chats

--- index.php
+++ index.php
@@ -8,5 +8,6 @@ $path = parse_url( $path, PHP_URL_PATH);
 Router::get('', 'DefaultController');
 Router::get('events', 'DefaultController');
 Router::get('people', 'DefaultController');
+Router::get('chats', 'DefaultController');
 
 Router::run($path);
--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -18,4 +18,9 @@ class DefaultController extends AppController {
     {
         $this->render('people');
     }
+
+    public function chats()
+    {
+        $this->render('chats');
+    }
 }
